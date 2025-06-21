5a7c0e96c904e09f8a2f0567595e0d2a73982846
nagy135 <viktor.nagy1995@gmail.com>
2025-06-21 15:05:39 +0200

    fix: remove old api structure

--- dto/async-job.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-import "github.com/nagy135/fitness-tracker/models"
-
-type AsyncJobDto struct {
-	Type models.AsyncJobType `json:"type" validate:"required,oneof=fetch-exercises"`
-}
--- dto/user.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type UserDto struct {
-	Name string `json:"name" validate:"required,min=3,max=50"`
-	Pass string `json:"pass" validate:"required,min=8,max=100"`
-} 
