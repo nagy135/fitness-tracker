4ddeeb1e22df2d146f16fc86330c13fce0e7e7a2
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-07-06 20:20:35 +0200

    fix: use of cors env

--- api/main.go
+++ api/main.go
@@ -39,7 +39,7 @@ func main() {
 
 	// Add middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3004",
+		AllowOrigins: cfg.Server.AllowOrigins,
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
